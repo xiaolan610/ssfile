be867b327f65f6359883814931b1b04d77d55b1e
lan <lan_at@163.com>
2023-08-10 17:07:44 +0800

    Update powershell美化.bat

--- powershell美化.bat
+++ powershell美化.bat
@@ -1,3 +1,3 @@
-powershell "irm https://ghproxy.com/https://raw.githubusercontent.com/xiaolan610/ssfile/file/for_powershell/clfpwsh.ps1 | iex"
 echo 如果弹出命令行对话框，请选A
-C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted clfpwsh.ps1
+powershell "irm https://ghproxy.com/https://raw.githubusercontent.com/xiaolan610/ssfile/file/for_powershell/clfpwsh.ps1 | iex"
+
