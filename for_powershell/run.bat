644d3cc5004bde3807c0ceb4199ced15b2a1a7b9
usertest <soft-diyuge@outlook.com>
2023-08-10 08:12:28 +0000

    6
    Signed-off-by: usertest <soft-diyuge@outlook.com>

---
+++ for_powershell/run.bat
@@ -0,0 +1,3 @@
+powershell curl https://gitee.com/supollad/shellfiles/raw/file/for_powershell/clfpwsh.ps1
+echo 如果弹出命令行对话框，请选A
+C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted clfpwsh.ps1
