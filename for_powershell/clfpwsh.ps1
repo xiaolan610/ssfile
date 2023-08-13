26763fe71d71be6533b0e8ce00716e5b28f444a7
usertest <soft-diyuge@outlook.com>
2023-08-13 03:18:27 +0000

    update for_powershell/clfpwsh.ps1.
    Signed-off-by: usertest <soft-diyuge@outlook.com>

--- for_powershell/clfpwsh.ps1
+++ for_powershell/clfpwsh.ps1
@@ -10,7 +10,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object
 oh-my-posh init
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 Install-Module ZLocation -Scope CurrentUser
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://ghproxy.com/https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1')
+irm https://ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/install.ps1 | iex
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
 scoop install aria2c 
@@ -18,7 +18,24 @@ scoop config aria2-retry-wait 4
 scoop config aria2-split 16
 scoop config aria2-max-connection-per-server 16
 scoop config aria2-min-split-size 4M
-scoop install git sudo python nano
+scoop bucket rm main
+scoop bucket add dorado https://ghproxy.com/https://github.com/chawyehsu/dorado
+scoop bucket add versions https://ghproxy.com/https://github.com/ScoopInstaller/Versions
+scoop bucket add extras https://ghproxy.com/https://github.com/ScoopInstaller/Extras
+scoop bucket add nerd-fonts https://ghproxy.com/https://github.com/matthewjberger/scoop-nerd-fonts
+scoop bucket add java https://ghproxy.com/https://github.com/ScoopInstaller/Java
+scoop bucket add nonportable https://ghproxy.com/https://github.com/TheRandomLabs/scoop-nonportable
+scoop bucket add nirsoft https://ghproxy.com/https://github.com/kodybrown/scoop-nirsoft
+scoop bucket add MorFans-apt https://ghproxy.com/https://github.com/Paxxs/Cluttered-bucket
+scoop bucket add spc https://ghproxy.com/https://github.com/lzwme/scoop-proxy-cn
+scoop config SCOOP_REPO https://ghproxy.com/github.com/ScoopInstaller/Scoop
+scoop bucket add main https://ghproxy.com/github.com/ScoopInstaller/Main
+scoop bucket add games https://ghproxy.com/https://github.com/Calinou/scoop-games
+scoop bucket add RandomLabs https://ghproxy.com/https://github.com/TheRandomLabs/Scoop-Bucket
+scoop install git sudo python nano yesplaymusic Meslo-NF-Mono Meslo-NF
+echo 你好，屏幕前的人，我想拜托你一件事情：请您先按下“Ctrl+,”快捷键，然后点击配置文件下的默认值，再点击外观选项，在字体栏上选择MesloLGL Nerd Font并回到这个标签页按下Enter键，谢谢
+echo 另外，如果报错请点击确定，并不用做任何事情，谢谢(●'◡'●)
+pause
 choco install node.js
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install rich cython pyfiglet
@@ -32,7 +49,7 @@ cd ~
 New-Item –Path $Profile –Type File
 mv Microsoft.PowerShell_profile.ps1 ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 .$profile
-rich -p "[green]基本完成"
-rich -p "[cyan]若想修改艺术字，请运行 [yellow]notepad $profile[cyan]并更新[yellow]python -m pyfiglet --color=LIGHT_CYAN [cyan]后的内容（别动引号）"
+rich -p "[green]基本完成╰(*°▽°*)╯"
+rich -p "[cyan]若想修改艺术字，请运行 [yellow]notepad $profile[cyan]并更新[yellow]python -m pyfiglet --color=LIGHT_CYAN [cyan]后的内容（别动引号）谢谢"
 
 
